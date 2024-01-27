1f07966a70d7cdaeedfa999134e3b36d6ddceefc
Andrew Walls <andrewwalls2003@gmail.com>
2024-01-27 14:10:37 -0500

    Correct starting waypoints

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,11 +15,11 @@ public class Constants {
         // Initial Points
         public static double FieldCenter[] = {0, 0, 0};
         public static double BlueStartingLeft[] = {0.610, 6.782, -120};
-        public static double BlueStartingCenter[] = {0.0, 0.0, -120};
-        public static double BlueStartingRight[] = {0.0, 0.0, -120};
-        public static double RedStartingLeft[] = {16.002, 4.369, -120};
-        public static double RedStartingCenter[] = {0, 0, -60};
-        public static double RedStartingRight[] = {15.951, 6.782, -60};
+        public static double BlueStartingCenter[] = {1.372, 5.639, 180};
+        public static double BlueStartingRight[] = {0.610, 4.369, 120};
+        public static double RedStartingLeft[] = {16.002, 4.369, 60};
+        public static double RedStartingCenter[] = {15.189, 5.639, 0};
+        public static double RedStartingRight[] = {16.002, 6.782, -60};
         
 
         // Auto Waypoints-
