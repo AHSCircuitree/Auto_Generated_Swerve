d73988d102a20dcf3e346aaf899d27fcd449c4fd
Andrew Walls <55559362+Flame875@users.noreply.github.com>
2024-02-12 15:17:59 -0500

    Logic fix

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -85,7 +86,7 @@ public class RobotContainer {
         .withRotationalRate((Deadband(-Player1.getRightX()) * MaxAngularRate)) 
     ));
 
-    Player1.leftTrigger().onTrue(new SequentialCommandGroup(new RunIntake(intake, .5), DriveToGamePiece()));
+    Player1.leftTrigger().onTrue(new ParallelCommandGroup(new RunIntake(intake, .5), DriveToGamePiece()));
 
     Player1.rightTrigger().onTrue(new RunShooter(arm, .5));
 
