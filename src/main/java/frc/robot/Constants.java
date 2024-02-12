7af793eef6045f1422df9c6d3bdbcd8cce506399
Andrew Walls <55559362+Flame875@users.noreply.github.com>
2024-02-12 12:37:21 -0500

    Color codes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,5 +87,43 @@ public class Constants {
         public static double MiddleRing4FromRed[] = {2.4384,8.001,270};
 
     }
-    
+
+    public class Colors {
+
+        // Fixed Patterns
+        public static double Rainbow = -.99;
+        public static double Ocean = -.95;
+        public static double Lava = -.93;
+        public static double Forest = -.91;
+
+        // Color Patterns
+        public static double ChaseColor = .01;
+        public static double HeartbeatColor = .05;
+
+        // Solid Colors
+        public static double HotPink = .57;
+        public static double DarkRed = .59;
+        public static double Red = .61;
+        public static double RedOrange = .63;
+        public static double Orange = .65;
+        public static double Gold = .67;
+        public static double Yellow = .69;
+        public static double LawnGreen = .71;
+        public static double Lime = .73;
+        public static double DarkGreen = .75;
+        public static double Green = .77;
+        public static double BlueGreen = .79;
+        public static double Aqua = .81;
+        public static double SkyBlue = .83;
+        public static double DarkBlue = .85;
+        public static double Blue = .87;
+        public static double BlueViolet = .89;
+        public static double Violet = .91;
+        public static double White = .93;
+        public static double Grey = .95;
+        public static double DarkGrey = .97;
+        public static double Black = .99;
+
+    }
+        
 }
